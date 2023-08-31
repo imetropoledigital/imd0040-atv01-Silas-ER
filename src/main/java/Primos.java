a43b78378c62aed51cca7b8e948c13a02461630f
Silas Eduardo <90927889+Silas-ER@users.noreply.github.com>
2023-08-31 13:45:58 +0000

    Atualização dos prints

--- src/main/java/Primos.java
+++ src/main/java/Primos.java
@@ -1,7 +1,7 @@
 public class Primos {
     public static void main(String[] args) {
         if (args.length < 1) {
-            System.out.println("Você deve passar um parâmetro.");
+            System.out.print("Você deve passar um parâmetro." + "\n");
             System.exit(0);
         }
         int qnt = Integer.parseInt(args[0]); //Converte para número a entrada
@@ -9,11 +9,11 @@ public class Primos {
         int numeroInicial = 2; //primeiro primo
 
         if(qnt == 1){
-            System.out.println(numeroInicial); //caso a entrada seja 1 já imprime o primeiro primo no caso o 2
+            System.out.print(numeroInicial + "\n"); //caso a entrada seja 1 já imprime o primeiro primo no caso o 2
         } else if (qnt == 0) {
-            System.out.println(" ");
+            System.out.print(" " + "\n");
         } else{
-            System.out.println(numeroInicial); //caso não seja imprime o primeiro primo
+            System.out.print(numeroInicial + "\n"); //caso não seja imprime o primeiro primo
             cont++; // o contador passa a ser um já que já apareceu o primeiro primo
             numeroInicial++; // o numero passa a ser 3 já que o 2 já foi impresso
             while(cont < qnt){ //coloquei no laço apenas condições necessárias para cumprir os testes
